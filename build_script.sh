ed7989253c66d1747bf8a0e4b502427210cadf25
root <root@Paul.>
2025-03-08 09:59:25 -0500

    Fix build path

--- build_script.sh
+++ build_script.sh
@@ -4,8 +4,8 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 echo "SCRIPT_DIR: $SCRIPT_DIR"  
 
 # Set paths relative to the script's directory
-BUILD_DIR="$(dirname "$SCRIPT_DIR")/vendor"
-BIN_DIR="$(dirname "$SCRIPT_DIR")/bin"
+BUILD_DIR="$SCRIPT_DIR/vendor"
+BIN_DIR="$SCRIPT_DIR/bin"
 
 # Build vendor directory
 mkdir -p "$BIN_DIR"
@@ -18,6 +18,7 @@ git clone https://github.com/weidai11/cryptopp.git
 git clone https://github.com/google/googletest.git
 git clone https://github.com/nlohmann/json.git
 git clone https://github.com/jbeder/yaml-cpp.git
+
 # Build dependencies
 cd "$BUILD_DIR/cadmium_v2"
 ./build.sh
@@ -29,6 +30,7 @@ cmake .
 make -j$(nproc)
 cd "$BUILD_DIR/cryptopp"
 make -j$(nproc)
+
 # Make current DEVS project
 cd "$PROJECT_ROOT"
 make build_test_raft
